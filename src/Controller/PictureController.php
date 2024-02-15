40cff0d91827aafd412355a833c7f7a4c700713a
- <karinateyssere@gmail.com>
2024-02-15 11:50:11 +0100

    Ajout fonctionnalité picture

---
+++ src/Controller/PictureController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Picture;
+use App\Repository\PictureRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
+
+class PictureController extends AbstractController
+{
+
+    #[Route('/', name: 'app_picture')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/PictureController.php'
+        ]);
+    }
+
+
+   /**
+     * Create new picture entry
+     * 
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $manager
+     * @param UrlGeneratorInterface $urlgenerator
+     * @return JsonResponse
+     */
+    #[Route('/api/picture', name: 'picture.create', methods: ['POST'])]
+    public function createPicture(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, UrlGeneratorInterface $urlgenerator): JsonResponse
+    {
+        $picture = new Picture();
+        $file = $request->files->get('file');
+        
+        $picture->setFile($file);
+        $picture->setMimeType($file->getClientMimeType());
+        $picture->setRealName($file->getClientOriginalName());
+        $picture->setName($file->getClientOriginalName());
+        $picture->setPublicPath('/public/medias/pictures');
+        $picture->setStatus('on')
+        ->setCreatedAt(new DateTime())
+        ->setUpdatedAt(new DateTime());
+
+        $entityManager->persist($picture);
+        $entityManager->flush();
+        
+        $jsonPicture = $serializer->serialize($picture, 'json');
+        $location = $urlgenerator->generate('picture.get',  ['idPicture'=>$picture->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonPicture, Response::HTTP_CREATED, ["Location" => $location], true);
+    }
+
+    /** 
+     * Renvoie l'entée picture
+     * 
+     * @param int $idPicture
+     * @param PictureRepository $repository
+     * @param UrlGeneratorInterface $urlgenerator
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
+    #[Route('/api/picture/{idPicture}', name: 'picture.get', methods: ['GET'])]
+    public function getPicture(int $idPicture, PictureRepository $repository, UrlGeneratorInterface $urlgenerator, SerializerInterface $serializer): JsonResponse
+    {
+        $picture = $repository->find($idPicture);
+        $location = $urlgenerator->generate('app_picture', [], UrlGeneratorInterface::ABSOLUTE_URL);
+        $location = $location . str_replace('/public/', "", $picture->getPublicPath())."/".$picture->getRealPath();
+
+        return $picture ?
+        new JsonResponse($serializer->serialize($picture,'json'), Response::HTTP_OK, ["Location" => $location],true) :
+        new JsonResponse(null, Response::HTTP_NOT_FOUND);
+    }
+
+}
---
+++ src/Entity/Picture.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Entity;
+
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use App\Repository\PictureRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+
+#[Vich\Uploadable]
+#[ORM\Entity(repositoryClass: PictureRepository::class)]
+class Picture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $realName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $realPath = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $publicPath = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $mimeType = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 24)]
+    private ?string $status = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $updatedAt = null;
+
+    #[Vich\UploadableField(mapping: "pictures", fileNameProperty:"realpath")]
+    private $file;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRealName(): ?string
+    {
+        return $this->realName;
+    }
+
+    public function setRealName(string $realName): static
+    {
+        $this->realName = $realName;
+
+        return $this;
+    }
+
+    public function getRealPath(): ?string
+    {
+        return $this->realPath;
+    }
+
+    public function setRealPath(string $realPath): static
+    {
+        $this->realPath = $realPath;
+
+        return $this;
+    }
+
+    public function getPublicPath(): ?string
+    {
+        return $this->publicPath;
+    }
+
+    public function setPublicPath(string $publicPath): static
+    {
+        $this->publicPath = $publicPath;
+
+        return $this;
+    }
+
+    public function getMimeType(): ?string
+    {
+        return $this->mimeType;
+    }
+
+    public function setMimeType(string $mimeType): static
+    {
+        $this->mimeType = $mimeType;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getFile(): ?File
+    {
+        return $this->file;
+    }
+
+    public function setFile(?File $file): ?Picture
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+}
